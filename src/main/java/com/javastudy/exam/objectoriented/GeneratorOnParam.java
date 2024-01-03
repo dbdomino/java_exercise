7a15076d7b1490c29e9d9a3704656ba246152fcc
dominod <dbdomino@naver.com>
2024-01-03 21:58:11 +0900

    this로 다른생성자 호출

--- src/main/java/com/javastudy/exam/objectoriented/GeneratorOnParam.java
+++ src/main/java/com/javastudy/exam/objectoriented/GeneratorOnParam.java
@@ -18,12 +18,13 @@ public class GeneratorOnParam {
     // 매개변수가 있는 생성자
     public static void main(String[] args) {
         Car c1 = new Car();
+    // 객체타입  객체이름 = 신규생성 기본생성자;
         c1.color    = "white";
         c1.gearType = "auto";
         c1.door = 4;
 
         Car c2 = new Car("white", "auto", 4);
-
+    // 객체타입  객체이름 = 신규생성 매개변수있는생성자;
         System.out.println("c1의 color=" + c1.color + ", gearType=" + c1.gearType+ ", door="+c1.door);
         System.out.println("c2의 color=" + c2.color + ", gearType=" + c2.gearType+ ", door="+c2.door);
     }
