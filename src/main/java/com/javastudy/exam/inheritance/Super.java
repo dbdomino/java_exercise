ce4466823f38cc3d8435d745d2478e514ab9b1f7
dominod <dbdomino@naver.com>
2024-01-04 22:13:17 +0900

    Overriding 메서드내용변경

--- src/main/java/com/javastudy/exam/inheritance/Super.java
+++ src/main/java/com/javastudy/exam/inheritance/Super.java
@@ -4,8 +4,12 @@ class Point {
     int x, y;
 
     Point() {
-    }
+        // 첫줄에 생성자 생략할 경우 컴파일러가 자동으로 super(); 를 넣어준다. 여기선 extends Object 이므로
+        // super(); => Object 클래스의 Object(); 가 해당될 것이다.
 
+    }
+// 이런식의 생성자를 하나 생성해두면, 기본생성자 Point()는 생성하지 않는다. 그러므로 다른데서 Point 클래스를 상속할 경우
+    // 생성자 부분에서 에러 발생할 수 있으므로 기본생성자는 왠만해선 직접 구현하자.
     Point(int x, int y) {
         this.x = x;
         this.y = y;
@@ -16,10 +20,13 @@ class Point3D extends Point {
     int z;
 
     Point3D() {
-        super(); // Point() 즉 부모의 생성자를 가져온다.
+//        super(); // 즉 부모의 생성자  Point()를 가져온다. Point 클래스에서 Point() {} 불러옴
+        //super();를 주석치면 컴파일 에러 발생, There is no default constructor available in 'com.javastudy.exam.inheritance.Point'
+        //자바의 특성임, 첫 줄에서 무조건 발생, 생략할 경우 super(); 를 컴파일러가 넣어준다.
     }
     Point3D(int x, int y, int z) {
-//        this(); // 자신의 다른 생성자를 가져오기도 가능하나, 생성자 가져오기는 첫 한줄만 가능
+//        this(); // 첫 줄에서 생성자 가져오기 무조건 발생 기입할 경우 기입대로 실행,
+//        생략할 경우 super(); 를 컴파일러가 넣어준다.
         super(x, y); // Point(int x, int y) 즉 부모의 생성자를 가져온다.
         this.z = z;
 
