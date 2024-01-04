99c20fb48794bddb4d76f06a90a0562a1724a01d
dominod <dbdomino@naver.com>
2024-01-04 22:26:17 +0900

    Package 정리

---
+++ src/main/java/com/javastudy/exam/packagedir/Package.java
@@ -0,0 +1,17 @@
+package com.javastudy.exam.packagedir;
+// 패키지는 소스파일의 첫 번째 문장으로 단 한번 선언.
+// 패키지
+// 서로 관련된 클래스의 묶음
+// 클래스는 클래스 파일(*.class), 패키지는 폴더, 하위 패키지는 하위 폴더를 의미
+// 클래스의 실제 이름(full name)은 패키지 명들을 포함한다.(java.lang.String 처럼)
+// rt.jar(runtime 을 위한 jar파일)는 클래스들을 압축한 파일(JDK경로\jre\lib에 위치함)
+
+// 같은 소스 파일의 클래스들은 모드 같은 패키지에 속하게 된다.
+// 패키지 선언이 없으면 이름없는(unnamed) 패키지에 속하게 된다.
+class Package01 {
+
+}class Package02 {
+
+}
+public class Package {
+}
