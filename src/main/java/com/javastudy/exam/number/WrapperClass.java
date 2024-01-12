a8fcf54d88fff26d7c0456f355fbbfd209fb35d8
dominod <dbdomino@naver.com>
2024-01-13 00:12:59 +0900

    wrapper 기본형을 참조형 객체로 다뤄야할때

---
+++ src/main/java/com/javastudy/exam/number/WrapperClass.java
@@ -0,0 +1,12 @@
+package com.javastudy.exam.number;
+
+// 8개의 기본형을 객체로 다뤄야할 때(참조형으로 다뤄야 할때) 사용하는 클래스
+public class WrapperClass {
+    public static void main(String[] args) {
+//        Boolean b = new Boolean(true); // 9버전부터 사용안함
+//        Character c = new Character('a'); // 9버전부터 사용안함
+//        Byte b = new Byte(12);
+//            Integer i = new Integer(100); // 9버전부터 사용안함.
+
+    }
+}
