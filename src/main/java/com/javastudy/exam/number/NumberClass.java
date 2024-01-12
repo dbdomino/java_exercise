a79cc5269f8b2554428df29bca1e3503d332f62e
dominod <dbdomino@naver.com>
2024-01-13 00:12:15 +0900

    number

---
+++ src/main/java/com/javastudy/exam/number/NumberClass.java
@@ -0,0 +1,12 @@
+package com.javastudy.exam.number;
+
+// Number 클래스
+// 모든 숫자 wrapper클래스의 조상
+public class NumberClass {
+    public static void main(String[] args) {
+//        Number n = new Number()
+        //
+
+    }
+
+}
