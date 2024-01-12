b406fbf288d4588f8558a7bd332a51c2d3f1827d
dominod <dbdomino@naver.com>
2024-01-12 15:02:54 +0900

    Object Value

--- src/main/java/com/javastudy/exam/object/ObjectValue.java
+++ src/main/java/com/javastudy/exam/object/ObjectValue.java
@@ -1,5 +1,7 @@
 package com.javastudy.exam.object;
 
+import java.util.Objects;
+
 public class ObjectValue {
     int value;
     ObjectValue() {
@@ -12,6 +14,14 @@ public class ObjectValue {
     public boolean equals(Object obj) {
         ObjectValue v = (ObjectValue) obj;// 다형성으로 형변환 지원
         return this.value == v.value;
+    }
+    // value의 값으로 hashcode출력하도록, equals도 값으로 비교하도록 수정했으니, HashCode도 값으로 HashCode화시킨걸 비교하기 위해
+    public int hashCode() {
+        return Objects.hashCode(value); //
+    }
+    // toString일 경우 인스턴스 변수가 출력되도록 overriding
+    public String toString() {
+        return "value:"+value;
     }
 
 }
