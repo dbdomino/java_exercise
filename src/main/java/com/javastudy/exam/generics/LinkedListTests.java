d9e1e74d337fe1f9d7da0b6aa28827133e1642fd
dominod <dbdomino@naver.com>
2024-01-17 12:04:51 +0900

    LinkedList 제네릭스

---
+++ src/main/java/com/javastudy/exam/generics/LinkedListTests.java
@@ -0,0 +1,11 @@
+package com.javastudy.exam.generics;
+
+import java.util.LinkedList;
+
+public class LinkedListTests {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        LinkedList<String> ll2 = new LinkedList();
+        LinkedList<Integer> ll3 = new LinkedList();
+    }
+}
