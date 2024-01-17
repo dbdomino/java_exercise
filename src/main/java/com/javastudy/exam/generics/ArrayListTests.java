0b5c1723ab00de46e317bbe8b60fa4cdd345888e
dominod <dbdomino@naver.com>
2024-01-17 12:03:15 +0900

    ArrayList 제네릭스 예제

---
+++ src/main/java/com/javastudy/exam/generics/ArrayListTests.java
@@ -0,0 +1,11 @@
+package com.javastudy.exam.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTests {
+    public static void main(String[] args) {
+        List<String> ls = new ArrayList<>();
+        List<Integer> li = new ArrayList<>();
+    }
+}
