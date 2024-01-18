a243ab9372a30a42173cf90c7a4b1c7f99a99d0c
dominod <dbdomino@naver.com>
2024-01-18 23:20:10 +0900

    wait notify 동기화지원

---
+++ src/main/java/com/javastudy/exam/thread/WaitNotifyEx.java
@@ -0,0 +1,120 @@
+package com.javastudy.exam.thread;
+
+import java.util.ArrayList;
+// 예제를 보면 wait와 notify로 스레드를 잠재우는건 좋은데, notify로 어떤 스레드를 깨울지 알 수 없다는게 문제다.
+public class WaitNotifyEx {
+    public static void main(String[] args) throws Exception {
+        Table table = new Table(); // 여러 쓰레드가 공유하는 객체
+
+        new Thread(new Cook(table), "COOK").start();
+        new Thread(new Customer(table, "donut"),  "CUST1").start();
+        new Thread(new Customer(table, "burger"), "CUST2").start();
+
+        Thread.sleep(8000);
+        System.exit(0);
+    }
+}
+class Customer implements Runnable {
+    private Table  table;
+    private String food;
+
+    Customer(Table table, String food) {
+        this.table = table;
+        this.food  = food;
+    }
+
+    public void run() {
+        while(true) {
+            try { Thread.sleep(10);} catch(InterruptedException e) {}
+            String name = Thread.currentThread().getName();
+
+            if(eatFood())
+                System.out.println(name + " ate a " + food);
+            else
+                System.out.println(name + " failed to eat. :(");
+        } // while
+    }
+
+    boolean eatFood() { return table.remove(food); }
+}
+
+class Cook implements Runnable {
+    private Table table;
+
+    Cook(Table table) {	this.table = table; }
+
+    public void run() {
+        while(true) {
+            int idx = (int)(Math.random()*table.dishNum());
+            table.add(table.dishNames[idx]);
+            try { Thread.sleep(100);} catch(InterruptedException e) {}
+        } // while
+    }
+}
+
+class Table {
+    String[] dishNames = { "donut","donut","burger" };
+    final int MAX_FOOD = 6;
+    private ArrayList<String> dishes = new ArrayList<>();
+    public synchronized void add(String dish) { // synchronized를 추가
+//        if(dishes.size() >= MAX_FOOD)
+//            return;
+//        dishes.add(dish);
+        while(dishes.size() >= MAX_FOOD) {
+            String name = Thread.currentThread().getName();
+            System.out.println("dishes.size() "+dishes.size()+" >=" +  MAX_FOOD+" MAX_FOOD");
+            System.out.println(name+"is waiting");
+            try {
+                wait();  // Cook 스레드를 기다리게 한다. 음식이 너무많으니
+                Thread.sleep(500);
+
+            } catch ( InterruptedException e) {}
+        }
+        dishes.add(dish);
+        notify(); // 기다리고있는  Cust를 깨움, 소비하기 위해, 그러나 과연 cust만 깨우는걸까?
+
+        System.out.println("Dishes:" + dishes.toString());
+    }
+
+    public boolean remove(String dishName) {
+        synchronized(this) {
+            String name = Thread.currentThread().getName();
+            while(dishes.size()==0) {
+                System.out.println(name+" is waiting.");
+//                try {
+//                    Thread.sleep(500);
+//                } catch(InterruptedException e) {}
+                try {
+                    wait();  // Cust를 기다리게 한다. 음식이 0개니까
+                    Thread.sleep(500);
+                } catch(InterruptedException e) {}
+            }
+
+//            for(int i=0; i<dishes.size();i++)
+//                if(dishName.equals(dishes.get(i))) {
+//                    dishes.remove(i);
+//                    return true;
+//                }
+            while (true) {
+                for (int i = 0; i < dishes.size(); i++) {
+                    if (dishName.equals(dishes.get(i))) {
+                        dishes.remove(i);
+                        notify(); // 잠자는 요리사를 깨운다. dish를 만들기 위해
+                        return true;
+                    }
+                }
+                try {
+                    System.out.println(name + " is waiting.");
+                    wait(); // 원하는 음식이 없는 Cust를 기다리게 한다.
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {}  // thread는 InterruptedException
+
+            } // while
+
+        } // synchronized
+//
+//        return false;
+    }
+
+    public int dishNum() { return dishNames.length; }
+}
