dc25946c2ae157bd393765175b9dc430e9da6970
dominod <dbdomino@naver.com>
2024-01-18 20:51:07 +0900

    IOblock 데몬스레드일경우 자동종료

--- src/main/java/com/javastudy/exam/thread/IOblocking.java
+++ src/main/java/com/javastudy/exam/thread/IOblocking.java
@@ -23,23 +23,35 @@ public class IOblocking  {
         // 위 작업을 멀티스레드로 변환
 
         Thread t = new Thread(new IOblockEX1());
-        t.setDaemon(true);		// 이 부분이 없으면 종료되지 않는다.
-        t.start();
+        IOblockEX1 t2 = new IOblockEX1();
+        t2.setDaemon(true);		// 이 부분이 없으면 종료되지 않는다. 데몬스레드로 설정, 메인메소드 종료되면 자동으로 데몬스레드도 종료됨.
+        t2.start();
+//        t.setDaemon(true);		// 이 부분이 없으면 종료되지 않는다. 데몬스레드로 설정, 메인메소드 종료되면 자동으로 데몬스레드도 종료됨
+//        t.start();	// 이 부분이 없으면 종료되지 않는다. 데몬스레드로 설정, 메인메소드 종료되면 자동으로 데몬스레드도 종료됨
+
 
         String input = JOptionPane.showInputDialog("아무값이나 입력하세요.");
+        t2.loopOn(false); // 이런식으로 데몬스레드를 수동으로 종료시키기 위해 무한루프를 스위치로 종료시키는 방법도 있다.
         System.out.println("입력한 값은 "+input + " 입니다.");
-        t.interrupt();  // 스레드 종료 시전
-//        t.isInterrupted(true);
 
+        String input3 = JOptionPane.showInputDialog("아무값이나 입력하세요.2");
     }
 }
 class IOblockEX1 extends Thread {
+    private boolean IOblockEX1_loop = true;   //stop플래그
     @Override
-    public void run() {
-        super.run();
-        for(int i = 10; i > 0; i --) {
+    public void run() { //
+//        super.run();
+        int i = 10;
+        while (IOblockEX1_loop){ // 무한루프
             System.out.println(i);;
             try {sleep(1000); } catch (Exception e) {}
+            i--;
         }
+        System.out.println("자원 정리");
+        System.out.println("IOblockEX1 실행 종료");
+    }
+    public void loopOn(boolean t) {
+        IOblockEX1_loop = t;
     }
 }
