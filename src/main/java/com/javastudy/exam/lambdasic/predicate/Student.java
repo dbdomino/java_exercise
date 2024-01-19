e8b698f88a72b4913bee53ffeb94fa1d689fa7c8
dominod <dbdomino@naver.com>
2024-01-19 13:38:31 +0900

    predicate 함수형인터페이스

---
+++ src/main/java/com/javastudy/exam/lambdasic/predicate/Student.java
@@ -0,0 +1,16 @@
+package com.javastudy.exam.lambdasic.predicate;
+
+public class Student {
+	private String name;
+	private String sex;
+	private int score;
+	
+	public Student(String name, String sex, int score) {
+		this.name = name;
+		this.sex = sex;
+		this.score = score;
+	}
+
+	public String getSex() { return sex; }
+	public int getScore() { return score; }
+}
