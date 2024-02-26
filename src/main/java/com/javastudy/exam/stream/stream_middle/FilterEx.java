56607fa54be7dc40f9f125ac2c1b5d49b8b71422
dominod <dbdomino@naver.com>
2024-02-26 13:14:27 +0900

    filter 예시

--- src/main/java/com/javastudy/exam/stream/stream_middle/FilterEx.java
+++ src/main/java/com/javastudy/exam/stream/stream_middle/FilterEx.java
@@ -2,6 +2,7 @@ package com.javastudy.exam.stream.stream_middle;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // Stream Filter 필터 예제
 public class FilterEx {
@@ -12,12 +13,33 @@ public class FilterEx {
 		names.stream()
 				.filter(n -> n.startsWith("가"))
 				.forEach(n -> System.out.println(n)); // 가로 시작된것만 가져오기
-		System.out.println();
+		System.out.println("-----");
+
+		names.stream()
+				.filter(n -> {
+					if (n.contains("나") ) return true; // true 반환 이라면 결과에 포함시킴.
+					else return false; // false반환 이라면 결과에 포함 시키지 않음.
+				})
+				.forEach(n -> System.out.println(n)); //
+
+		System.out.println("-----");
 
 		names.stream()
 				.distinct()
 				.filter(n -> n.startsWith("다"))
 				.filter(n -> n.startsWith("나"))
 				.forEach(n -> System.out.println(n)); // 다, 나로 시작된것만 가져오기, 없음
+
+		System.out.println("-----");
+
+		List<String> result = names.stream()
+				.filter(n -> {
+					if (n.contains("나")) return true; // true 반환 이라면 결과에 포함시킴.
+					else return false; // false반환 이라면 결과에 포함 시키지 않음.
+				})
+				.toList();//
+//				.collect(Collectors.toList());
+		System.out.println("List  : "+result);
+
 	}
 }
