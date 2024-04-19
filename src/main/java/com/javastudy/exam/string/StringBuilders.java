b34819d3d6fbf2a2227a56b988ceb13a9f2ff0f2
dominod <dbdomino@naver.com>
2024-04-19 19:02:40 +0900

    String Builder

--- src/main/java/com/javastudy/exam/string/StringBuilders.java
+++ src/main/java/com/javastudy/exam/string/StringBuilders.java
@@ -5,4 +5,14 @@ package com.javastudy.exam.string;
 // StringBuilder는 비동기화, 단일 스레드에서 사용권장
 // StringBuffer는 동기화 지원, 멀티 스레드에서 안전하다. 단일스레드에 써도 크게상관은 없다만 성능이슈있으면 StringBuilder로 바꿔줘야함.(위에가 성능더좋음)
 public class StringBuilders {
+
+    public static void main(String[] args) {
+        String s = "ABCDEFG!";
+
+        String answer = "";
+
+        answer = new StringBuilder(s).reverse().toString();
+
+        System.out.println(answer);
+    }
 }
